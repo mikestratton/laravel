8abf85ab283b85cda1da7e6e6251abed7779b154
mikestratton <mike@mikestratton.net>
2017-06-02 04:08:44 -0400

    Laravel Fundamentals: Database, Eloquent/ORM

---
+++ cms/app/Post.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+
+    protected $date = ['deleted_at'];
+
+
+/*
+|------------------------------------
+| MassAssignmentException
+|------------------------------------
+*/
+    protected $fillable = [
+        'title',
+        'content',
+    ];
+
+/*
+|------------------------------------
+| class name does not match with table name
+| for example, class AdminPost defaults to table adminposts
+|------------------------------------
+*/
+//    protected $table = 'posts';
+
+/*
+|------------------------------------
+| by default primary key defaults to 'id'
+| change primary key name with protected
+|------------------------------------
+*/
+//    protected $primaryKey = 'post_id';
+}
