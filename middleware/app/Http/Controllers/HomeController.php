56ceb349c1d8ecc0107e7e15a7994ab0007fe1eb
mikestratton <mike@mikestratton.net>
2017-06-16 01:52:27 -0400

    sessions

--- middleware/app/Http/Controllers/HomeController.php
+++ middleware/app/Http/Controllers/HomeController.php
@@ -20,10 +20,25 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
+     * @param Request $request
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        /* sessions */
+//        $request->session()->put(['mike'=>'senior developer']);
+//        $request->session()->get('mike');
+//        session(['tina'=>'ceo']); // global function
+//        $request->session()->forget('mike'); delete mike session
+//        $request->session()->flush(); // delete all
+//        return session()->all();
+
+        /* flashing data */
+//        $request->session()->flash('message', 'Post has been created');
+//        return $request->session()->get('message');
+//        $request->session()->reFlash(); // keeps data longer
+//        $request->session()->keep('message'); // keeps data longer
+
         return view('home');
     }
 }
